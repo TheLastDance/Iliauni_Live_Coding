7c766d5a9d4efbdb8f629f0dfd38fdd9eb836d42
unknown <davit.khachaturov.1@iliauni.edu.ge>
2023-11-20 23:59:07 +0400

    added eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
       { allowConstantExport: true },
     ],
     'no-unused-vars': 'warn',
-    'react-hooks/exhaustive-deps': 'off'
+    'react-hooks/exhaustive-deps': 'off',
+    "react/prop-types": 'off'
   },
 }
